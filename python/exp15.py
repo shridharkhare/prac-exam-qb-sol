ade432348a067c11036d794179698328994ae3b6
shridharkhare <kshreedhar01622@gmail.com>
2024-10-27 05:30:26 +0530

    refactor: refactor python exp 15 to make it a packagae based instead of module based program

---
+++ python/exp15.py
@@ -0,0 +1,21 @@
+import exp15.exp15c_mod as c
+import exp15.exp15sc_mod as sc
+
+
+def main():
+    print(c.add(101, 202))
+    print(c.sub(101, 202))
+    print(c.mul(101, 202))
+    print(c.div(101, 202))
+    print(c.sqrt(30))
+    print(sc.sin(30))
+    print(sc.cos(30))
+    print(sc.tan(30))
+    print(sc.log(30))
+    print(sc.log10(30))
+    print(sc.exp(30))
+    print(sc.factorial(5))
+    print(sc.power(2, 3))
+
+
+main()
